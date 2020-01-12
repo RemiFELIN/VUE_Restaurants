82c0c415b245453c37619e9ae187b50da7b61cc1
loubard <yannick.cardini@gmx.com>
2020-01-12 20:44:56 +0100

    ajout fichier modalMaps

---
+++ client/src/components/modalMaps.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="modalMaps">
+    <modal name="remiNeSaitPasAffichierUneModale" v-if="currentRestaurant"><ul v-if="currentRestaurant.address.coord">
+      <li>Latitude:{{currentRestaurant.address.coord[0]}}</li>
+      <li>Longitude:{{currentRestaurant.address.coord[1]}}</li>
+      </ul>
+      <div v-if="!currentRestaurant.address.coord">Ce restaurant n'a pas de Longitude/latitude</div>
+    </modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "modalMaps",
+  props: {
+    msg: {}
+  },
+  watch: {
+    msg: function() {
+      this.currentRestaurant = this.msg;
+    }
+  },
+  data() {
+    return {
+      currentRestaurant: null
+    };
+  }
+};
+</script>
